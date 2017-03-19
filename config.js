bc0800f09f016da4b6d8f327f7b0f78caf71f31d
JoniR <rasanenj@gmail.com>
2017-03-19 13:00:31 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+/*
+Config file for external configurations like MongoDb server address. I decide to use this instead of Heroku Configuration and Config Vars to ensure that future deployment of different platform in future is ease.
+
+Please note that this file is listed on .gitignore
+*/
+
+var config = {};
+
+config.MONGODB_URI = '';
